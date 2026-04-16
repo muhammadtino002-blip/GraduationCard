76495587c0c721b00b4ceb5327f3de2cd8f83906
amikom <muhammadtino002@gmail.com>
2026-04-16 14:35:23 +0700

    Add Background

--- app/src/main/java/com/example/graduationcard/MainActivity.kt
+++ app/src/main/java/com/example/graduationcard/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,10 +16,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.graduationcard.R.string.HappyGraduationText
+import com.example.graduationcard.R.string.HopeText
+import com.example.graduationcard.R.string.SenderText
 import com.example.graduationcard.ui.theme.GraduationCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,10 +35,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             GraduationCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreatingText(
-                        massage = "Happy Graduation Tino!",
-                        hope = "Semoga Sukses Selalu",
-                        sender = "From Desta",
+                    GreatingImage(
+                        massage = stringResource(HappyGraduationText),
+                        hope = stringResource(HopeText),
+                        sender = stringResource(SenderText),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -40,8 +48,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun GreatingImage(massage: String, hope: String, sender: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.background)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+
+        GreatingText(
+            massage = massage,
+            hope = hope,
+            sender = sender,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
 fun GreatingText(massage: String, hope:  String, sender: String,  modifier: Modifier = Modifier) {
-    Column (verticalArrangement = Arrangement.Center, modifier = Modifier.padding(8.dp).fillMaxSize()) {
+    Column (verticalArrangement = Arrangement.Center, modifier = Modifier
+        .padding(8.dp)
+        .fillMaxSize()) {
         Text(
             text = massage,
             fontSize = 60.sp,
@@ -52,13 +83,17 @@ fun GreatingText(massage: String, hope:  String, sender: String,  modifier: Modi
         Text(
             text = hope,
             fontSize = 25.sp,
-            modifier = Modifier.padding(all = 16.dp).align(Alignment.CenterHorizontally)
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(Alignment.CenterHorizontally)
         )
 
         Text(
             text = sender,
             fontSize = 28.sp,
-            modifier = Modifier.padding(all = 16.dp).align(Alignment.End)
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(Alignment.End)
         )
     }
 }
@@ -67,6 +102,6 @@ fun GreatingText(massage: String, hope:  String, sender: String,  modifier: Modi
 @Composable
 fun GreetingPreview() {
     GraduationCardTheme {
-        GreatingText("Happy Graduation Tino!", sender = "From Desta", hope = "Semoga Sukses Selalu")
+        GreatingImage(massage = "Happy Graduation Tino", hope = "Semoga Sukses Selalu", sender = " From Tino")
     }
 }
